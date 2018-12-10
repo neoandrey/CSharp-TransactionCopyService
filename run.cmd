ac0f28bb23c94dd2f3439243f4ac5849a545af9d
neoandrey@yahoo.com <neoandrey@yahoo.com>
2018-12-10 23:10:19 +0100

    Change run script to PowerShell

--- run.cmd
+++ run.cmd
@@ -2,9 +2,9 @@
 git  branch
 echo "Do you want to commit the application on the current  branch? (type  'y' or  'n'): "
 @echo off 
-set /p commit_response=  
+set /p commit_response1=  
 
-if  %commit_response%==y (
+if  %commit_response1%==y (
   @echo off 
   echo 'Please type commit  message: 
   set /p commit_message= 
@@ -24,9 +24,9 @@ call set branch=%%astr:%substr1%=%substr2%%%
 if NOT %branch%==master (
  echo "Do you want to merge to the local master branch? (type  'y' or  'n'):"
 @echo off 
-set /p commit_response=  
+set /p commit_response2=  
 
-if  [%commit_response%]==y (
+if  [%commit_response2%]==y (
 
  git merge %branch%
 
@@ -34,35 +34,39 @@ if  [%commit_response%]==y (
 )
 
  echo "Do you want to push to the remote master branch? (type  'y' or  'n'):"
- set /p commit_response=
-if  %commit_response%==y (
+ set /p commit_response3=
+if  %commit_response3%==y (
     echo "checking saved remote repo..."
     IF EXIST "%cd%\remote_repo.txt" (
-      set /p url= <.\remote_repo.txt
-      echo "loaded repo: "+ %url%
+      set /p url=<"%cd%\remote_repo.txt"
+      echo  %url%
+      @echo off 
   ) ELSE (
   echo "Please type the URL of the remote repository: "
     set /p url=
-    if  [%url%] neq [] (
-        echo "saving remote %url%"
-      echo %url%>%cd%\remote_repo.txt
+    if  [%url%] neq [] ( 
+        echo "saving remote repo:
+        echo %url%>"%cd%\remote_repo.txt"
     ) )
 git remote>%cd%\tmpFile
-set /p remote_repo_name= <%cd%\tmpFile
-del /f %cd%\tmpFile
+set /p remote_repo_name=<"%cd%\tmpFile"
+del /f "%cd%\tmpFile"
 set  bool=F
-if [%remote_repo_name%]==[]  set  bool=T
+if [%remote_repo_name%]==[]  set bool=T
 if [%remote_repo_name%]==n  set  bool=T
-     echo "isOriginSet: %bool%"
+@echo on
+     echo   %bool%
+     @echo off 
  if  [%bool%]==T (
 
    echo "git remote add origin %url%"
    git remote add origin %url%
   )
   echo "Please type the name of the branch you wish to push: " 
-  set /p branch= 
-  echo "Running: git push -u origin %branch% "
-  git push -u origin %branch%  
+  set /p push_branch= 
+  echo "Running: git push -u origin  "%push_branch%
+  git push -u origin %push_branch%  
+   echo "checking remote branch: " 
   git remote 
 
 )
---
+++ run.ps1
@@ -0,0 +1,74 @@
+﻿git branch
+ $shouldCommit = Read-Host -Prompt "Do you want to commit the application on the current  branch? (type  'y' or  'n')"
+ while ($shouldCommit -eq $null){
+    $shouldCommit = Read-Host -Prompt "Do you want to commit the application on the current  branch? (type  'y' or  'n')"
+ 
+ }
+ if  ($shouldCommit -eq 'y'){
+   
+
+        $commitMessage=    Read-Host -Prompt  'Please type commit  message'
+         git add .
+         git commit -m $commitMessage
+    
+ }
+
+ cd  $pwd
+ # $pwd\bin\TranCopy.exe
+
+ $current_branch_string = Get-Content ".git/HEAD"
+ $current_branch        =  $current_branch_string.Replace("ref: refs/heads/","")
+ 
+ if ($current_branch -ne "master" ){
+    $branch_response =  Read-Host -Prompt "Do you want to merge to the local master branch? (type  'y' or  'n')"
+     while ($branch_response -eq $null){
+      $branch_response =  Read-Host -Prompt "Do you want to merge to the local master branch? (type  'y' or  'n')"
+     }
+
+    if ($branch_response  -eq 'y'){
+        Write-Host "Running:  git merge  $current_branch"
+        git merge  $current_branch 
+    } 
+ }
+
+ $push_response=  Read-Host -Prompt "Do you want to push to the remote master branch? (type  'y' or  'n')"
+ while ($push_response -eq $null){
+  $push_response=  Read-Host -Prompt "Do you want to push to the remote master branch? (type  'y' or  'n')"
+ }
+ if($push_response -eq 'y' ){
+   Write-Host 'Checking saved remote repo...'
+   $filepath ="$pwd\remote_repo.txt"
+   if(![System.IO.File]::Exists($filepath)){
+        $remote_repo_url = Get-Content $filepath
+      }
+
+      if($remote_repo_url -eq $null){
+          $remote_repo_url  =  Read-Host -Prompt  "Please type the URL of the remote repository"
+
+           while ($remote_repo_url -eq $null){
+              $remote_repo_url  =  Read-Host -Prompt  "Please type the URL of the remote repository"
+
+           }
+
+           echo $remote_repo_url | Out-File -FilePath "$pwd\remote_repo.txt"
+        }
+
+        $remote_repo_name = git remote
+
+        if($remote_repo_name -eq $null -Or $remote_repo_name -eq 'n'){
+        
+          Write-Host "git remote add origin $remote_repo_url"
+          git remote add origin $remote_repo_url
+
+        }
+       
+        $push_branch =  Read-Host -Prompt  "Please type the name of the branch you wish to push" 
+         while ($push_branch -eq $null){
+            $push_branch =  Read-Host -Prompt  "Please type the name of the branch you wish to push" 
+         }
+         git push -u origin $push_branch
+         Write-Host "checking remote branch: " 
+         git remote 
+
+
+    }
