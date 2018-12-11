609454e8d9e79e7158e83a19cbdb393de79b49ea
neoandrey@yahoo.com <neoandrey@yahoo.com>
2018-12-11 14:35:49 +0100

    update run script to set proxy

--- run.cmd
+++ run.cmd
@@ -1,14 +1,14 @@
  @echo off
- set /p proxy_path=< "%cd%\proxy.txt"
- echo "proxy_path: %proxy_path%"
- echo "Checking proxy path..."
- IF EXIST "%proxy_path%" (
-   SET /p proxy_string=< %proxy_path%
+ set proxy_filepath=%cd%\proxy.txt
+ echo "Checking proxy path: %proxy_filepath%"
+ IF EXIST "%proxy_filepath%" (
+   SET /p proxy_url=<"%proxy_filepath%"
  )  ELSE (
    echo "Please type proxy URL:"
       set /p proxy_url=
       echo  %proxy_url%>%cd%\proxy.txt
  )
+ echo "setting proxy as:%proxy_url%"
  SET  HTTP_PROXY=%proxy_url%
  SET  HTTPS_PROXY=%proxy_url%
 powershell  -File  %cd%\run.ps1
